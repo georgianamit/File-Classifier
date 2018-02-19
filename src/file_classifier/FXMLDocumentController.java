25cc78c878cff19201d4546ac86591df6646b4c3
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-19 21:52:33 +0530

    edited the code

--- src/file_classifier/FXMLDocumentController.java
+++ src/file_classifier/FXMLDocumentController.java
@@ -7,25 +7,14 @@ package file_classifier;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
  *
  * @author spider
  */
 public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- src/file_classifier/File_Classifier.java
+++ src/file_classifier/File_Classifier.java
@@ -22,6 +22,7 @@ public class File_Classifier extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+
         
         stage.setScene(scene);
         stage.show();
