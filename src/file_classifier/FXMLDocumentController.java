3db17a900814859bb620312e87ed22cafe7d3e31
Amit Rawat <georgianamitrawat@gmail.com>
2018-02-23 23:40:13 +0530

    working with FileChooser

--- src/file_classifier/FXMLDocumentController.java
+++ src/file_classifier/FXMLDocumentController.java
@@ -5,20 +5,44 @@
  */
 package file_classifier;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  *
  * @author spider
  */
 public class FXMLDocumentController implements Initializable {
+    @FXML
+    private Label lbl;
+    FileChooser fileChooser = new FileChooser();
 
+
+    
+    @FXML
+    private void browseBtnAction(ActionEvent event) {
+         File selectedDir = fileChooser.showOpenDialog(null);
+         
+         
+         if(selectedDir != null){
+             if(selectedDir.isDirectory()){
+                 System.out.print("its ok");
+             }
+         }else{
+             
+         }
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        fileChooser.setTitle("Choose Folder");
     }    
     
 }
